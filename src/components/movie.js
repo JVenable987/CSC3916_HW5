ca61ea9d99b0fe168c552078e9f4f1d5685031de
JVenable987 <joshua.j.venable@gmail.com>
2020-04-19 20:26:20 -0600

    React and patterns Video commit

--- src/components/movie.js
+++ src/components/movie.js
@@ -1,9 +1,59 @@
 import React, {Component} from 'react';
 import {connect} from "react-redux";
+import {Glyphicon, Panel, ListGroup, ListGroupItem} from "react-bootstrap";
+import {Image} from "react-bootstrap";
 import { withRouter } from "react-router-dom";
+import {fetchMovie} from "../actions/movieActions";
+//import PanelBody from "react-bootstrap/lib/PanelBody";
 
 class movie extends Component{
+    componentDidMount() {
+        const {dispatch} = this.props;
+        if(this.props.selectedMovie == null){
+            dispatch(fetchMovie(this.props.movieId))
+        }
+    }
 
+    render() {
+        const ActorInfo = ({actors}) => { //change based on api
+            return actors.map((actor, i ) =>
+                <p key={i}>
+                    <b>{actor.actorName}</b> {actor.characterName}
+                </p>
+            )
+        }
+
+        const ReviewInfo = ({reviews}) => { //change based on api
+            return reviews.map((review, i) =>
+                <p key={i}>
+                    <b>{review.username}</b> {review.review}
+                    <Glyphicon glyph={'star'} /> {review.rating}
+                </p>
+            )
+        }
+
+        const DetailInfo = ({currentMovie}) => {
+            if(!currentMovie){
+                return <div>Loading...</div>;
+            }
+            return (
+                <Panel>
+                    <Panel.Heading>Movie Detail</Panel.Heading>
+                    <Panel.Body><Image className = "Image" src={currentMovie.imageUrl} thumbnail /></Panel.Body>
+                    <ListGroup>
+                        <ListGroupItem>{currentMovie.title}</ListGroupItem>
+                        <ListGroupItem><ActorInfo actors={currentMovie.actors} /></ListGroupItem>
+                        <ListGroupItem><h4><Glyphicon glyph={'star'}/>{currentMovie.avgRating}</h4></ListGroupItem>
+                    </ListGroup>
+                    <Panel.Body><ReviewInfo reviews={currentMovie.reviews} /></Panel.Body>
+                </Panel>
+            );
+        }
+
+        return (
+            <DetailInfo currentMovie={this.props.selectedMovie} />
+        );
+    }
 }
 
 const mapStateToProps = (state, ownProps) => {
